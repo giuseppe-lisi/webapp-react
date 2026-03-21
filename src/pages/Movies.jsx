1c83d9e4f06246ffc8b54087ca18c563ec52cda0
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 15:52:20 +0100

    aggiunto film da passare alla card

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,16 +1,20 @@
 import Card from "../components/Card";
 
 function Movies() {
+    const movie = {
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made"
+    };
+
     return (
         <>
             <div className="card-container wrapper">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card movie={movie}/>
             </div>
         </>
-    )
+    );
 }
 
 export default Movies;
