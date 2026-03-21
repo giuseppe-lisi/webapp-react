1d57120db315f167c05f3bd07cc4361321b2bd59
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 16:34:18 +0100

    aggiunta navbar all'header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <header className="wrapper">
-            <h1>Header del sito!</h1>
+            <h1>Movie.js</h1>
+            <nav className="header-nav">
+                <Link to="/movies">Lista film</Link>
+                <Link to="/">Homepage</Link>
+            </nav>
         </header>
     );
 }
