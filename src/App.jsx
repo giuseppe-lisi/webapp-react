2693b5e6b3caa39bfbd4fa572da5bb5819be89a3
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 15:21:14 +0100

    creato router e definite le rotte

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import Header from "./pages/Header";
+import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
+
 import Movies from "./pages/Movies";
+import Homepage from "./pages/Homepage";
+import MovieDetail from "./pages/MovieDetail";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
     return (
-        <>
-            <Header />
-            <Movies />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route Component={DefaultLayout}>
+                    <Route path="/" Component={Homepage}></Route>
+                    <Route path="movies" Component={Movies}></Route>
+                    <Route path="movies/:id" Component={MovieDetail}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
