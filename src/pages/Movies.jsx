21dfa2227f6684ce436838f09a82e37ad5359406
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 14:35:41 +0100

    aggiunto component che contiene le card dei film

---
+++ src/pages/Movies.jsx
@@ -0,0 +1,25 @@
+function Movies() {
+    return (
+        <>
+            <div className="card-container wrapper">
+                <div className="card">
+                    card
+                </div>
+                <div className="card">
+                    card
+                </div>
+                <div className="card">
+                    card
+                </div>
+                <div className="card">
+                    card
+                </div>
+                <div className="card">
+                    card
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Movies;
