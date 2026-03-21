e8a787940a53a529ccfd2574aa4255af8d313a9d
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 15:20:29 +0100

    creata pagina homepage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+function Homepage() {
+    return(
+        <>
+            <h2>Benvenuto in Movie.js</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, eligendi fuga? Vel veritatis unde corporis repellendus, tempore officiis doloribus! Magnam culpa eum mollitia, consequatur enim accusantium ratione quibusdam doloribus possimus.</p>
+        </>
+    )
+}
+
+export default Homepage
