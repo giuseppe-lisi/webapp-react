60db1a63ae8be375d63174d2e6899dc1e1128402
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 16:34:09 +0100

    review e film sono in due file js separati

---
+++ src/data/movies.js
@@ -0,0 +1,67 @@
+const movies = [
+    {
+        id: 1,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 2,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 3,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 4,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 5,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 6,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 7,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 8,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+    {
+        id: 9,
+        title: "Matrix",
+        img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+        year: "1999",
+        desc: "Best movie ever made",
+    },
+];
+
+export default movies;
