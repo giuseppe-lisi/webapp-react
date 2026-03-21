9a533650b169fbff56d8a78956a11cc4611ac6e4
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 14:35:21 +0100

    migliorato header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,9 @@
 function Header() {
-    <>
-        <h1>Header del sito!</h1>
-    </>
+    return (
+        <header className="wrapper">
+            <h1>Header del sito!</h1>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
