a1f215f79adadecb0dbc943efbcf3e86c1e1f76f
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 15:24:28 +0100

    aggiunto component card

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+function Card() {
+    return (
+        <div className="card">
+            <div>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
+                    alt="immagine film"
+                />
+            </div>
+            <div>Titolo Film</div>
+            <div>
+                <p>Descrizione film</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
