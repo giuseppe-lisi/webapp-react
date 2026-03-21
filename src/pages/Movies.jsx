b6c97df40949f1aba822ebf88a39fcff9b777675
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-21 16:35:15 +0100

    spostato array dei film in un file separato e aggiornato componente card che viene mostrato in pagina

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,77 +1,13 @@
-import Card from "../components/Card";
+import CardMovie from "../components/CardMovie";
+import movies from "../data/movies";
 
 function Movies() {
-    const movie = [
-        {
-            id: 1,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 2,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 3,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 4,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 5,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 6,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 7,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 8,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        },
-        {
-            id: 9,
-            title: "Matrix",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
-            year: "1999",
-            desc: "Best movie ever made"
-        }
-    ];
 
     return (
         <>
             <div className="card-container wrapper">
-                {movie.map((movie) => {
-                    return <Card movie={movie} key={movie.id}/>;
+                {movies.map((movie) => {
+                    return <CardMovie movie={movie} key={movie.id}/>;
                 })}
             </div>
         </>
