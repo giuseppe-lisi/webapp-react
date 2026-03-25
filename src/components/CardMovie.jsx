76f3bb331d88a68a0e652494bc2d76fdd16d33e0
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-25 13:14:31 +0100

    refactor card dei film per accettare i dati recuperati dal db

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -2,17 +2,16 @@ import { Link } from "react-router-dom";
 import MovieDetail from "../pages/MovieDetail";
 
 function Card({ movie }) {    
-    const { id, title, img, year, desc} = movie;
+    const {title, abstract, release_year, image} = movie;
 
     return (
         <div className="card">
             <div>
-                <img src={img}/>
+                <img src={image}/>
             </div>
             <h3>{title}</h3>
-            <p>{year}</p>
-            <p>{desc}</p>
-            <Link to={`/movies/${id}`}>Vai al dettaglio</Link>
+            <p>{release_year}</p>
+            <p>{abstract}</p>
         </div>
     );
 }
