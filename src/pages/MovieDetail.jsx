25c2deac66dc5d17e6a4d1658bc078b3596823a4
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-25 14:21:24 +0100

    aggiunta chiamata axios per mostrare il dettaglio del film, incluse le recensioni

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,21 +1,36 @@
 import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import CardReview from "../components/CardReview";
 
 function MovieDetail() {
+    const { id } = useParams();
+
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:3000/api/movies/${id}`)
+            .then((res) => {
+                setMovie(res.data);
+            })
+            .catch((err) => console.log("Qualcosa non va: ", err.message));
+    }, []);
+
     return (
         <>
             <div className="dettaglio">
-                <h2>Dettaglio film</h2>
-                <h3>Title</h3>
-                <p>Desc</p>
+                <img src={movie.image} />
+                <h3>{movie.title}</h3>
+                <h2>{movie.release_year}</h2>
+                <p>{movie.genre}</p>
+                <p>{movie.abstract}</p>
                 <Link to="/movies/">Torna alla lista dei film</Link>
             </div>
-
-            <h2>Recensioni</h2>
-            <div className="card-container wrapper">
-                {reviews.map((review) => {
-                    return <CardReview reviewData={review} key={review.id} />;
-                })}
+            <div className="card-container">
+                {movie.reviews?.map((review) => (
+                    <CardReview review={review} key={review.id}/>
+                ))}
             </div>
         </>
     );
