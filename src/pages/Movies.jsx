62177aeb08bfaff9dbbac4cb95530bf9b3590c5e
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-03-25 13:15:16 +0100

    la pagina della lista dei film effettua ora una chiama tramite axios che recupera i film dal backend e li visualizza tramite card in pagina

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,27 @@
 import CardMovie from "../components/CardMovie";
-import movies from "../data/movies";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function Movies() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/api/movies")
+            .then((res) => {
+                setMovies(res.data);
+                console.log(res.data);
+            })
+            .catch((err) =>
+                console.error("Qualcosa è andato storto: ", err.message),
+            );
+    }, []);
 
     return (
         <>
             <div className="card-container wrapper">
                 {movies.map((movie) => {
-                    return <CardMovie movie={movie} key={movie.id}/>;
+                    return <CardMovie movie={movie} key={movie.id} />;
                 })}
             </div>
         </>
